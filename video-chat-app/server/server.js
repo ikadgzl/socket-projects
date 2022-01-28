044fea4b2716ce5800bca40ac11e7ccb0e3ca7dd
ikadgzl <ikadgzl@gmail.com>
2022-01-28 06:44:44 +0300

    added socket logic

--- video-chat-app/server/server.js
+++ video-chat-app/server/server.js
@@ -19,6 +19,22 @@ app.get('/', (req, res) => {
   res.send('hey');
 });
 
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callended');
+  });
+
+  socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('calluser', { signal: signalData, from, name });
+  });
+
+  socket.on('answercall', (data) => {
+    io.to(data.to).emit('callaccepted', data.signal);
+  });
+});
+
 httpServer.listen(PORT, () => {
   console.log(`Server is up and running on ${PORT}`);
 });
